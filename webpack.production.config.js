2edc1137703ce89373e320e984eed89b695a3b3d
Lucas <lucas@agtlucas.com>
2016-01-20 23:14:57 -0200

    Updates

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,35 +8,42 @@ var lost = require('lost')
 var rucksack = require('rucksack-css')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var StatsPlugin = require('stats-webpack-plugin')
 
-console.log('Development!')
+console.log('Production!')
 
 module.exports = {
-  devtool: 'eval-source-map',
   entry: [
-    'webpack-hot-middleware/client?reload=true',
     path.join(__dirname, 'src/js/app.js')
   ],
 
   output: {
-    path: path.join(__dirname, '/dist/'),
+    path: path.join(__dirname, '/dist'),
     filename: 'index.js',
     publicPath: '/'
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index-dev.html',
+      template: 'src/index.html',
       inject: 'body',
-      filename: 'index-dev.html'
+      filename: 'index.html'
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_END': JSON.stringify('production')
     }),
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('[name]-[hash].min.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    })
   ],
 
   module: {
@@ -48,9 +55,10 @@ module.exports = {
       test: /\.styl$/,
       loader: 'style-loader!css-loader!postcss-loader!stylus-loader'
     }, {
-      test: /\.(png|jpe?g|svg|gif)$/,
+      test: /.*\.(gif|png|jpe?g|svg|gif)$/i,
       loaders: [
-        'url-loader?limit=8192'
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
       ]
     }],
     resolve: {
